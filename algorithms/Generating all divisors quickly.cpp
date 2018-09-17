6548fd9bcad4b230f02d11934d68a1eacf7b6ef5
Abdelrhman Mahfouz <abdomahfoz5@gmail.com>
2018-09-17 21:59:43 +0200

    Generate all divisors quickly

---
+++ algorithms/Generating all divisors quickly.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int Range = 1000000;
+int spf[Range + 5];
+
+void GeneratePrimes()
+{
+    for(int i = 2; i <= Range; i++)
+    {
+        spf[i] = i;
+    }
+    for(int i = 2; i <= Range; i++)
+    {
+        if(spf[i] == i)
+        {
+            for(int j = i; (long long int)j * i <= Range; j++)
+            {
+                if(spf[j * i] == j * i)
+                {
+                    spf[j * i] = i;
+                }
+            }
+        }
+    }
+}
+
+int FastPow(int base, int power)
+{
+    if(power == 0)
+    {
+        return 1;
+    }
+    if(power == 1)
+    {
+        return base;
+    }
+    if(power % 2 == 0)
+    {
+        int x = FastPow(base, power / 2);
+        return x * x;
+    }
+    return base * FastPow(base, power - 1);
+}
+
+void multiplyFactors(int i, int val, const vector<pair<int, int>>& factors, vector<int>& divisors)
+{
+    if(i == factors.size())
+    {
+        divisors.push_back(val);
+        return;
+    }
+    for(int j = 0; j <= factors[i].second; j++)
+    {
+        multiplyFactors(i+1, val * FastPow(factors[i].first, j), factors, divisors);
+    }
+}
+
+void GetDivisors(int n, vector<int>& divisors)
+{
+    vector<pair<int, int>> factors;
+    while(n > 1)
+    {
+        if(factors.empty() || factors[factors.size() - 1].first != spf[n])
+        {
+            factors.push_back({spf[n], 1});
+        }
+        else
+        {
+            factors[factors.size() - 1].second++;
+        }
+        n /= spf[n];
+    }
+    multiplyFactors(0, 1, factors, divisors);
+    sort(divisors.begin(), divisors.end());
+}
+
+vector<vector<int>> allDivisors;
+
+int main()
+{
+    GeneratePrimes();
+    allDivisors.resize(Range + 5);
+    for(int i = 1; i <= Range; i++)
+    {
+        GetDivisors(i, allDivisors[i]);
+    }
+    return 0;
+}
